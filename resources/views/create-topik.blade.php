3a19f4fbea419996ba4830a5f7bc8a2259b6cf3a
Sabila Malahati A <bellaarnindya@gmail.com>
2017-08-03 08:14:28 +0700

    tambah topik [hapus diskusi blm bener]

--- resources/views/create-topik.blade.php
+++ resources/views/create-topik.blade.php
@@ -21,21 +21,45 @@
                             <div class="form-group">
                                 <label for="Topik" class="col-sm-2 control-label">Topik <span style="color: red">*</span></label>
 
-                                <div class="col-sm-9">
+                                <div class="col-sm-8">
                                     <input type="text" class="form-control" name="topik" placeholder="Topik" required>
                                 </div>
                             </div>
-                            <hr>
-                            <div class="form-group">
-                                <label for="HasilDiskusi" class="col-sm-2 control-label">Hasil Diskusi</label>
+                            <div id="form-section">
+                                <div id="discussion-coy" class="wadu">
+                                    <div id="discussion-section" style="display: none">
+                                        <br>
+                                        <br>
+                                        <hr>
+                                        <div class="form-group">
+                                            <label for="HasilDiskusi" class="col-sm-2 control-label">Hasil Diskusi</label>
 
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" name="diskusi[][]" placeholder="Hasil Diskusi">
+                                            <div class="col-sm-8">
+                                                <input type="text" class="form-control" name="diskusi[0]" placeholder="Hasil Diskusi" diskusi="0">
+                                            </div>
+                                            <div class="col-sm-2">
+                                                <button type="button" class="btn btn-default" id="delete-discussion-button" style="margin-left:10px; margin-top: 0px"><i class="fa fa-times"></i></button>
+                                            </div>
+                                        </div>
+                                        <div id="discussion-action">
+                                            <div id="action-section">
+                                                <div class="form-group">
+                                                    <label for="Action" class="col-sm-2 control-label">Action</label>
+
+                                                    <div class="col-sm-8">
+                                                        <textarea class="form-control" name="action[0][]" placeholder="Action"></textarea>
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <button type="button" class="btn btn-default" id="delete-action-button" style="margin-left:10px; margin-top: 10px"><i class="fa fa-times"></i></button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button type="button" class="btn btn-default pull-right" id="add-action-button" style="margin-right: 20px">Tambah Action</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="input-field">
-                            </div>
-                            <button type="button" class="btn btn-default add-button pull-right"style="margin-right: 3px">Tambah Hasil Diskusi</button>
+                            <button type="button" class="btn btn-default pull-right" id="add-discussion-button" style="margin-right: 3px">Tambah Hasil Diskusi</button>
                         </div>
                         <!-- /.box-body -->
                         <div class="box-footer">
@@ -51,27 +75,61 @@
     </div>
     <script>
         $(document).ready(function(){
-            var max_fields=10;
-            var x=0;
-            $(".add-button").click(function(e){ //on add input button click
+
+            const MAX_FIELDS = 10;
+            var totalDiscussion = 1;
+            var ind = 0;
+            var totalAction = 1;
+            var discussionSectionClone = $("#discussion-section")[0].outerHTML.replace('<button type="button" class="btn btn-default" id="delete-discussion-button" style="margin-left:10px; margin-top: 0px"><i class="fa fa-times"></i></button>','').replace('diskusi[0]', 'diskusi['+totalDiscussion+']').replace('action[0][]', 'action['+totalDiscussion+'][]').replace('style="display: none"', '').replace('diskusi="0"', 'diskusi="'+ind+'"');
+            const formContent = $('#form-section');
+            var discussionActionFieldsClone = $('#action-section')[0].outerHTML.replace('<button type="button" class="btn btn-default" id="delete-action-button" style="margin-left:10px; margin-top: 10px"><i class="fa fa-times"></i></button>');
+            const actionFieldsClone = $('#discussion-action')[0].outerHTML;
+
+            if (totalDiscussion==1) {
+                $(formContent).append(discussionSectionClone);
+                totalDiscussion++;
+                ind++;
+            }
+
+            $('#add-discussion-button').on('click', function(e){ //on add input button click
                 e.preventDefault();
-                if(x < max_fields){ //max input box allowed
-                    x++; //text box increment
-                    $(".input-field").append('<div class="form-group"><label for="HasilDiskusi" class="col-sm-2 control-label">Hasil Diskusi</label><div class="col-sm-9"><input type="text" class="form-control" name="diskusi[][]" placeholder="Hasil Diskusi"></div><div class="col-sm-1"><a href="#" class="remove_field"><button class="btn btn-default"><i class="fa fa-times"></i></button></a></div></div><div class="action-field'+x+'"></div>');
+                discussionSectionClone = $("#discussion-section")[0].outerHTML.replace('diskusi[0]', 'diskusi['+totalDiscussion+']').replace('action[0][]', 'action['+totalDiscussion+'][]').replace('style="display: none"', '').replace('diskusi="0"', 'diskusi="'+ind+'"');
+//                console.log(discussionSectionClone);
+                if( totalDiscussion < MAX_FIELDS){ //max input box allowed
+                    $('#form-section').append(discussionSectionClone);
+                    totalDiscussion++;
+                    ind++;
                 }
             });
 
-            $(".add-action-button").click(function(e){ //on add input button click
-                e.preventDefault();
-                if(x < max_fields){ //max input box allowed
-                    x++; //text box increment
-                    $(".action-field").append('<div class="form-group"><label for="HasilDiskusi" class="col-sm-2 control-label">Hasil Diskusi</label><div class="col-sm-9"><input type="text" class="form-control" name="diskusi[][]" placeholder="Hasil Diskusi"></div><div class="col-sm-1"><a href="#" class="remove_field"><button class="btn btn-default"><i class="fa fa-times"></i></button></a></div></div>');
-                }
+            $(document).on('click', '#add-action-button', function(e) {
+                var btnParent = $(e.target.parentNode.childNodes[9]);
+                var nthDiscussion = $(e.target.parentNode.childNodes[7].childNodes[3].childNodes[1]).attr('diskusi');
+                console.log(nthDiscussion);
+                discussionActionFieldsClone = $('#action-section')[0].outerHTML.replace('action[0][]', 'action['+nthDiscussion+'][]');
+                totalAction++;
+//                console.log($(btnParent));
+
+                btnParent.append(discussionActionFieldsClone);
             });
 
-            $(".input-field").on("click",".remove_field", function(e){ //user click on remove text
-                e.preventDefault(); $(this).parents()[1].remove(); x--;
-            })
+            $(document).on('click', '#delete-action-button', function(e) {
+                var btnParent = $(e.target.parentNode.parentNode.parentNode);
+                totalAction--;
+                console.log(btnParent);
+                btnParent.remove();
+            });
+
+            $(document).on('click', '#delete-discussion-button', function(e) {
+                var btnParent = $(e.target).parents();
+//                if (btnParent[0].includes("</i>")) console.log('iiiii');
+//                else console.log('bbbb');
+                totalDiscussion--;
+//                console.log($(e.target).parents());
+                console.log(btnParent);
+//                btnParent.remove();
+            });
+
         })
 
 
