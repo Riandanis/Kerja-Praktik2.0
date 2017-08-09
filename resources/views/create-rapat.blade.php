0e13b74206634fa9103c8b95eb8375c1f5ff8517
dimashirda <dimas0308@gmail.com>
2017-08-09 12:06:24 +0700

    add delete atendee

--- resources/views/create-rapat.blade.php
+++ resources/views/create-rapat.blade.php
@@ -23,38 +23,41 @@
                             <div class="form-group">
                                 <label for="HeadlineRapat" class="col-sm-2 control-label">Headline Rapat <span style="color: red">*</span></label>
 
-                                <div class="col-sm-10">
+                                <div class="col-sm-8">
                                     <input type="text" class="form-control" placeholder="Headline Rapat" name="headline" required>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="DateRapat" class="col-sm-2 control-label">Tanggal Rapat </label>
 
-                                <div class="col-sm-10">
+                                <div class="col-sm-8">
                                     <input type="date" class="form-control" placeholder="YYYY-MM-DD" name="tanggal_rapat" required>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="TimeRapat" class="col-sm-2 control-label">Waktu Rapat</label>
 
-                                <div class="col-sm-10">
+                                <div class="col-sm-8">
                                     <input type="time" class="form-control" placeholder="HH:MM" name="waktu_rapat" required>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="TempatRapat" class="col-sm-2 control-label">Tempat Rapat</label>
 
-                                <div class="col-sm-10">
+                                <div class="col-sm-8">
                                     <input type="text" class="form-control" placeholder="Tempat Rapat" name="tempat" required>
                                 </div>
                             </div>
-                            <div id="attendee-section">
-                                <div class="form-group" id="dynamicInput">
+                            <div id="attendee-section" >
+                                <div class="form-group" id="dynamicInput" style="display: none">
                                     <label for="AttendeeRapat" class="col-sm-2 control-label">Attendee</label>
 
-                                    <div class="col-sm-10">
+                                    <div class="col-sm-8">
                                         <input type="text" class="form-control" placeholder="Attendee" name="peserta[]" required>
                                     </div>
+                                    <div class="col-sm-2">
+                                        <button type="button" class="btn btn-default fa fa-times" id="delete-attendee-button" style="margin-left:10px; margin-top: 0px; height: 34px"></button>
+                                    </div>  
                                 </div>
                             </div>
 
@@ -83,16 +86,33 @@
         $(document).ready(function(){
             var totalAttendee=0;
             var max_field = 99;
-            var newdiv = $("#dynamicInput")[0].outerHTML.replace('<label for="AttendeeRapat" class="col-sm-2 control-label">Attendee</label>', '<label for="AttendeeRapat" class="col-sm-2 control-label"></label>').replace('<div class="form-group" id="dynamicInput">', '<div class="form-group">');
+            var newdiv = $("#dynamicInput")[0].outerHTML
+            .replace('<label for="AttendeeRapat" class="col-sm-2 control-label">Attendee</label>', '<label for="AttendeeRapat" class="col-sm-2 control-label"></label>')
+            .replace('<div class="form-group" id="dynamicInput">', '<div class="form-group">')
+            .replace('style="display: none"', '');
             var parentDiv = $("#attendee-section");
 
+            if (totalAttendee==0) {
+                var firstDiv = $("#dynamicInput")[0].outerHTML
+            .replace('<div class="form-group" id="dynamicInput">', '<div class="form-group">')
+            .replace('<button type="button" class="btn btn-default fa fa-times" id="delete-attendee-button" style="margin-left:10px; margin-top: 0px; height: 34px"></button>', '')
+            .replace('style="display: none"', '');
+             $(parentDiv).append(firstDiv);
+                    totalAttendee++;
+            }
             $("#add-attendee-button").on('click', function(e){
                 e.preventDefault();
                 if(totalAttendee < max_field) {
                     $(parentDiv).append(newdiv);
                     totalAttendee++;
                 }
-            })
+            });
+
+             $(document).on('click', '#delete-attendee-button', function(e) {
+                var btnParent = $(e.target).parents()[1];
+                totalAttendee--;
+                btnParent.remove();
+            });
 
         })
 
