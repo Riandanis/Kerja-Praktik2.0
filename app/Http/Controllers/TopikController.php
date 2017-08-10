53969a0cc2693097c3fc3f807480805ad1f25004
ranitera <raniaulia72@gmail.com>
2017-08-10 14:35:36 +0700

    topik masih error

--- app/Http/Controllers/TopikController.php
+++ app/Http/Controllers/TopikController.php
@@ -171,12 +171,216 @@ class TopikController extends Controller
         $date = $request->input('due_date');
         $nama_topik = $request->input('topik');
         $diskusi = $request->input('diskusi');
-        dd($diskusi);
-        //foreach($diskusi as $d)
+        // dd($action);
+        if(count($diskusi)==1 and $diskusi[0]==null){
+            // dd($diskusi);
+            $d = new Diskusi;
+            $d->id_topik = $id_topik;
+            $d->nama_diskusi = 'Tidak Ada Diskusi';
+            $d->save();
+            $id_d = $d->id_diskusi;
 
+            $a = new Action;
+            $a->id_diskusi = $id_d;
+            $a->deskripsi = 'Tidak Ada Action';
+            $a->jenis_action = 'Informasi';
+            $a->email_pic = 'example@email.com';
+            $a->status = 1;
+            $a->save();
+        }
+        else{
+            $i = 0;
+            $dis = array();
+            foreach($diskusi as $d){
+                if(array_key_exists($i, $diskusi)){
+                    if($d!=null){
+                        $edit_d = Diskusi::where('nama_diskusi', $d)
+                                ->where('id_topik', $id_topik)->first();
 
+                        if($edit_d == null){
+                            $new = new Diskusi();
+                            $new->id_topik = $id_topik;
+                            $new->nama_diskusi = $d;
+                            if($new->save()){
+                                $flag = 1;
+                                $id_d = $new->id_diskusi;
+                            }
+                            else{
+                                $flag = 0;
+                            }
+                        }
+                        else{
+                            $id_d = $edit_d->id_diskusi;
+                        }
 
+                        //ACTION
+                        $aksi = 0;
+                        if($i == 0){
+                            $j =1;
+                        }
+                        else{
+                            $j = 0;
+                        }
+                        
+                        $cek = array();
+                        if(array_key_exists($i, $action)){
+                            // dd(count($action[$i]));
+                            if($i == 0 and $action[$i][1]==null){
+                                // dd('HEHE');
+                                $a = new Action;
+                                $a->id_diskusi = $id_d;
+                                $a->deskripsi = 'Tidak Ada Action';
+                                $a->jenis_action = 'Informasi';
+                                $a->email_pic = 'example@email.com';
+                                $a->due_date = null;
+                                $a->status = 1;
+                                $a->save();
+                                $aksi++;
+                            }
+                            else if(count($action[$i]==1 and $action[$i][0]==null)){
+                                //dd('HEHE');
+                                $a = new Action;
+                                $a->id_diskusi = $id_d;
+                                $a->deskripsi = 'Tidak Ada Action';
+                                $a->jenis_action = 'Informasi';
+                                $a->email_pic = 'example@email.com';
+                                $a->due_date = null;
+                                $a->status = 1;
+                                $a->save();
+                                $aksi++;
+                            }
+                            else{
+                                foreach($action[$i] as $act){
+                                    if(array_key_exists($j, $action[$i])){
+                                        if($act!=null){
+                                            $edit_a = Action::where('id_diskusi', $id_d)
+                                                    ->where('deskripsi', $act)
+                                                    ->first();
 
+                                            if($edit_a == null){
+                                                //bikin baru
+                                                $acti = new Action();
+                                                $acti->id_diskusi = $id_d;
+                                                $acti->deskripsi = $act;
+                                                if($jenis[$i][$j] == 'Target'){
+                                                    $acti->jenis_action = 'Target';
+                                                    $acti->due_date = $date[$i][$j];
+                                                    $acti->email_pic = $pic[$i][$j];
+                                                    $acti->status = 0;
+                                                }
+                                                else{
+                                                    $acti->jenis_action = 'Informasi';
+                                                    $acti->due_date = null;
+                                                    $acti->email_pic = 'example@email.com';
+                                                    $acti->status = 1;
+                                                }
+
+                                                if($act->save()){
+                                                    $aksi++;
+                                                    $flag = 1;
+                                                }
+                                                else{
+                                                    $flag = 0;
+                                                }
+
+                                            }
+                                            else{
+                                                //update action
+                                                if($jenis[$i][$j] == 'Target'){
+                                                    $edit_a->jenis_action = 'Target';
+                                                    $edit_a->due_date = $date[$i][$j];
+                                                    $edit_a->email_pic = $pic[$i][$j];
+                                                    $edit_a->status = 0;
+                                                }
+                                                else{
+                                                    $edit_a->jenis_action = 'Informasi';
+                                                    $edit_a->due_date = null;
+                                                    $edit_a->email_pic = 'example@email.com';
+                                                    $edit_a->status = 1;
+                                                }
+                                                if($edit_a->save()){
+                                                    $aksi++;
+                                                    $flag = 1;
+                                                }
+                                                else{
+                                                    $edit_a = 0;
+                                                }
+
+                                            }
+                                        }
+                                    }
+
+                                    if($act!=null){
+                                        array_push($cek, $act);
+                                    }
+                                    
+                                    $j++;
+                                }
+                            }
+                        }
+                        else{
+                            $a = new Action;
+                            $a->id_diskusi = $id_d;
+                            $a->deskripsi = 'Tidak Ada Action';
+                            $a->jenis_action = 'Informasi';
+                            $a->email_pic = 'example@email.com';
+                            $a->due_date = null;
+                            $a->status = 1;
+                            $a->save();
+                            $aksi++;
+                        }
+
+                        if($aksi == 0){
+                            $a = new Action;
+                            $a->id_diskusi = $id_d;
+                            $a->deskripsi = 'Tidak Ada Action';
+                            $a->jenis_action = 'Informasi';
+                            $a->email_pic = 'example@email.com';
+                            $a->due_date = null;
+                            $a->status = 1;
+                            $a->save();
+                        }
+
+                    }
+
+                } 
+
+                if($d!=null){
+                    array_push($dis, $d);
+                }
+
+                $dba = Action::where('id_diskusi', $id_d)
+                        ->whereNotIn('deskripsi', $cek)
+                        ->get();
+
+                foreach($dba as $h){
+                    if($h->delete()){
+                        $flag = 1;
+                    }
+                    else{
+                        $flag = 0;
+                    }
+                }
+
+                $i++;
+
+            }
+        }
+        
+        // dd($aksi);
+        // $db = Diskusi::where('id_topik', $id_topik)
+        //         ->whereNotIn('nama_diskusi', $dis)
+        //         ->get();
+
+        foreach($db as $ha){
+            if($ha->delete()){
+                $flag = 1;
+            }
+            else{
+                $flag = 0;
+            }
+        }
+        // dd($flag);
     }
 
     public function destroy(Request $request, $id)
